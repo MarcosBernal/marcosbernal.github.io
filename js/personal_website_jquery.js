1fc016dc7f2ee8fe233e083d057f8e51238c9909
MarcosBernal <marcosbernal3@hotmail.com>
2017-09-26 01:12:44 +0200

    Improved dynamically overlaping with click and drag, improved behavior of buttons and fixed bug with text.

--- js/personal_website_jquery.js
+++ js/personal_website_jquery.js
@@ -22,23 +22,45 @@ $(document).ready(function(){
     var divPos = {};
     var right_graph_pos = 0;
     var offset = $("#left_text").offset();
-    $(".switching_content").on("mouseover mousemove", function(e){
-        divPos = {
-            left: e.pageX - offset.left,
-            top: e.pageY - offset.top
-        };
-        right_graph_pos = $('#left_text').width()-divPos.left;
-        $("#right_graph").css('width',right_graph_pos);
-        $("#divider").css('left',divPos.left+$('#divider').width());
+    // $(".switching_content").on("mouseover mousemove", function(e){
+    //     divPos = {
+    //         left: e.pageX - offset.left,
+    //         top: e.pageY - offset.top
+    //     };
+    //     right_graph_pos = $('#left_text').width()-divPos.left;
+    //     $("#right_graph").css('width',right_graph_pos);
+    //     $("#divider").css('left',divPos.left+$('#divider').width());
+    // });
+    $(".switching_content").on("mousemove", function(e){
+    divPos = {
+        left: e.pageX - offset.left,
+        top: e.pageY - offset.top
+    };
+    });
+    var interval;
+    $("#divider").on("mousedown touchstart", function(e){
+        interval = setInterval(function() {
+            divPos.left = divPos.left > 0 ? divPos.left : 0;
+            divPos.left = divPos.left > $('#left_text').width()+$('#divider').width() ? $('#left_text').width()+$('#divider').width() : divPos.left;
+            right_graph_pos = $('#left_text').width() - divPos.left;
+            $("#right_graph").css('width', right_graph_pos);
+            $("#divider").css('left', divPos.left + $('#divider').width());
+            $("p").css('user-select', "none");
+        }, 20);
+    });
+    $("#divider").on("mouseup touchend", function(){
+        clearInterval(interval);
+        $("p").css('user-select', "auto");
     });
 
     //Show completely all introduction either text or graph
-    $("#text_button").on("click", function(e){
+    $("#text_button").on("click touch", function(e){
         $("#right_graph").css('width',0);
-        $("#divider").css('left',$("#left_text").width()+$("#divider").width()*2);
+        $("#divider").css('left',$("#left_text").width()+$("#divider").width());
+
     });
 
-    $("#graph_button").on("click", function(e){
+    $("#graph_button").on("click touch", function(e){
         $("#right_graph").css('width',$("#left_text").width());
         $("#divider").css('left',0+$("#divider").width());
     });
