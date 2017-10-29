ab2ac8b2b4a39f924eff014ee467911846a0811e
MarcosBernal <marcosbernal3@hotmail.com>
2017-10-29 05:09:40 +0100

    Improved slightly the html file. Fixed temporally bug with divider and graph canvas size annoyingly attached to mouse (need to improve)

--- js/personal_website_jquery.js
+++ js/personal_website_jquery.js
@@ -2,13 +2,12 @@
 $(document).ready(function(){
 
     // Set position and size to graph and divider initially in the middle
-    $("#divider").css({'left':($('#left_text').width()/2)+10,
-                       'height':$('#left_text').height()
-                      });
-    $("#divider").offset({top:$("#left_text").offset().top});
     $("#right_graph").offset({top:$("#left_text").offset().top});
     $("#right_graph").css({'width':Math.round($('#left_text').width()/2),
-                           'height':$('#left_text').height()});
+        'height':$('#left_text').height()});
+    $("#divider").css({'right':($('#right_graph').width()+$('#divider').width()),
+        'height':$('#left_text').height()});
+    $("#divider").offset({top:$("#left_text").offset().top});
 
     $("#mynetwork").css({'width':$('#left_text').width(),
         'height':$('#left_text').height()});
@@ -32,34 +31,40 @@ $(document).ready(function(){
         top: e.pageY - offset.top
     };
     });
-    var interval;
+    var interval = undefined;
     $("#divider").on("mousedown touchstart", function(e){
-        interval = setInterval(function() {
-            divPos.left = divPos.left > 0 ? divPos.left : 0;
-            divPos.left = divPos.left > $('#left_text').width()+$('#divider').width() ? $('#left_text').width()+$('#divider').width() : divPos.left;
-            right_graph_pos = $('#left_text').width() - divPos.left;
-            $("#right_graph").css('width', right_graph_pos);
-            $("#divider").css('left', divPos.left + $('#divider').width());
-            $("p").css('user-select', "none");
-            $("img").css('user-select', "none");
-
-        }, 20);
+        if(interval == undefined){
+            interval = setInterval(function() {
+                divPos.left = divPos.left > 0 ? divPos.left : 0;
+                divPos.left = divPos.left > $('#left_text').width()+$('#divider').width() ? $('#left_text').width()+$('#divider').width() : divPos.left;
+                right_graph_pos = $('#left_text').width() - divPos.left;
+                $("#right_graph").css('width', right_graph_pos);
+                $("#divider").css('right',$('#right_graph').width()+$('#divider').width());
+                $("p").css('user-select', "none");
+                $("img").css('user-select', "none");
+            }, 20);
+        }
     });
     $("#divider").on("mouseup touchend", function(){
         clearInterval(interval);
+        interval = undefined;
         $("p").css('user-select', "auto");
         $("img").css('user-select', "auto");
     });
 
     //Show completely all introduction either text or graph
     $("#text_button").on("click touch", function(e){
+        clearInterval(interval);
+        interval = undefined;
         $("#right_graph").css('width',0);
-        $("#divider").css('left',$("#left_text").width()+$("#divider").width());
+        $("#divider").css('right',$("#right_graph").width()+$("#divider").width());
 
     });
 
     $("#graph_button").on("click touch", function(e){
+        clearInterval(interval);
+        interval = undefined;
         $("#right_graph").css('width',$("#left_text").width());
-        $("#divider").css('left',0+$("#divider").width());
+        $("#divider").css('right',$("#right_graph").width()+$("#divider").width());
     });
 });
