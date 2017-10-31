110e771921ab4f4772f98356ed95091f76a4b4d8
MarcosBernal <marcosbernal3@hotmail.com>
2017-11-01 00:01:01 +0100

    Updated README.md file. Inited first version of graph

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # Personal website
 
-This is my personal website developed from the Template made by W3School.
+This is the repository of my personal website. If you are curious, you can [take a look of it](http://marcosbernal.es) 
 
-In construction
+#### - In construction: It will be update little by little -
+
+# Use of libraries
+
+I found these libraries very powerful and, as result, I decided to use them.
+
+- [jquery](https://jquery.com/): No intro needed
+- [Font Awesome](https://github.com/FortAwesome/Font-Awesome): just awesome, like their icons
+- [w3.css](https://www.w3schools.com/w3css/): whereas the w3.css is not a library but a framework it is going to be here added here. BTW, useful 
+- [Visjs](http://visjs.org/): To create the graph and it interaction within the canvas 
+- [Twitter Post Fetch](https://github.com/jasonmayes/Twitter-Post-Fetcher): Get tweets is an easy task, doing that without oauth is another matter
+
 
 # Credits
 
-Some ideas were taken from the project fullPage.js and the free template Parallax
-
-More information in:
-   - https://alvarotrigo.com/fullPage/
+As everybody, I also took ideas from the world including several projects and websites.
+To mention a few:
+   - http://www.adhamdannaway.com  
+   - https://alvarotrigo.com/fullPage
    - https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_templates_parallax&stacked=h
--- js/visjs_network_graph.js
+++ js/visjs_network_graph.js
@@ -1,18 +1,43 @@
 // create an array with nodes
 var nodes = new vis.DataSet([
-    {id: 1, label: 'Node 1'},
-    {id: 2, label: 'Node 2'},
-    {id: 3, label: 'Node 3'},
-    {id: 4, label: 'Node 4'},
-    {id: 5, label: 'Node 5'}
+    {id: 1, label: 'Marcos'},
+    {id: 2, label: 'Bernal España'},
+    {id: 3, label: 'Spaniard'},
+    {id: 4, label: 'Data Science Student'},
+    {id: 5, label: 'Technical School of Madrid(UPM)'},
+    {id: 6, label: 'Interests'},
+    {id: 7, label: 'Computer Engineering'},
+    {id: 8, label: 'Data Science'},
+    {id: 9, label: 'Problem Solving Person'},
+    {id: 10, label: 'Hockey'},
+    {id: 11, label: 'Networking'},
+    {id: 12, label: 'Security'},
+    {id: 13, label: 'Software Development'},
+    {id: 14, label: 'Robotics'},
+    {id: 15, label: 'Data Mining'},
+    {id: 16, label: 'Machine Learning'},
+
+
+
 ]);
 
 // create an array with edges
 var edges = new vis.DataSet([
-    {from: 1, to: 3},
-    {from: 1, to: 2},
-    {from: 2, to: 4},
-    {from: 2, to: 5}
+    {from: 1, to: 2, label:"familyName"},
+    {from: 1, to: 3, label:"nationality"},
+    {from: 1, to: 4, label:"hasOccupation"},
+    {from: 4, to: 5, label:"alumniOf"},
+    {from: 1, to: 6, label:"seeks"},
+    {from: 6, to: 7, label:"like"},
+    {from: 6, to: 8, label:"like"},
+    {from: 1, to: 9, label:"summary"},
+    {from: 1, to: 10, label:"plays"},
+    {from: 7, to: 11, label:"belongsTo"},
+    {from: 7, to: 12, label:"belongsTo"},
+    {from: 7, to: 13, label:"belongsTo"},
+    {from: 7, to: 14, label:"belongsTo"},
+    {from: 8, to: 15, label:"belongsTo"},
+    {from: 8, to: 16, label:"belongsTo"}
 ]);
 
 // create a network
