eefc3a7836dbf1becf1dc4916515bbc508e4aaf2
MarcosBernal <marcosbernal3@hotmail.com>
2018-01-01 14:21:18 +0100

    Added multilingual version with Spanish and English. Improved script and index file

--- multilingual_division.sh
+++ multilingual_division.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+
+# Moving to the folder path
+cd $(pwd)/$(dirname $0)
+
 if [ ! -f dislines.pl ]; then
 echo 'There is no file dislines.pl. Text can not be split. Download the file first'
 exit 1
@@ -10,12 +14,20 @@ exit 1
 fi
 
 echo "Everything is ready. Starting..."
-cd $(pwd)/$(dirname $0)
 
 ./dislines.pl index.MULTILINGUAL.html
 
-echo "Renaming files"
+echo " -> Renaming index"
 mv index.MULTILINGUAL.en.html index.html
 mv index.MULTILINGUAL.es.html index.es.html
+
+echo " -> Processing additional_content"
+cd additional_content
+find *MULTILINGUAL.html | xargs -n 1 ../dislines.pl
+
+echo " -> Renaming additial_content"
+for file in *MULTILINGUAL.en.html; do mv "$file" "${file%.MULTILINGUAL.en.html}.html"; done
+for file in *MULTILINGUAL.es.html; do mv "$file" "${file%.MULTILINGUAL.es.html}.es.html"; done
+
 
 echo "Finished creation and rename of files"
